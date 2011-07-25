5f1d421935694e3a7c3222c1ed4dd50a5544ae92
Lemurien Mignon <lemur@elephant.(none)>
2011-07-25 22:43:15 +0200

    static and dynamic

--- widgetfb.cc
+++ widgetfb.cc
@@ -6,53 +6,135 @@
 #include <QBrush>
 #include <QDebug>
 #include <QColor>
+#include <QMenu>
 
 #define WIDTH 1000
 #define HEIGHT 400
-#define OUTTERRADIUS 80
-#define OUTTERWIDTHMIN 20
-#define OUTTERWIDTHMAX 50
-#define INNERRADIUS 50
-#define KERN 10
 #define NBEATS 4
 
-static const QColor colors[] = {
+static const QColor colors[NBEATS] = {
     QColor::fromCmykF(1,0,0,0),
     QColor::fromCmykF(0,1,0,0),
     QColor::fromCmykF(0,0,1,0),
     QColor::fromCmykF(0,0,0,1)
 };
 
-void drawPad(QPainter &painter,int index,int x,int y,bool state, int width) {
+#define OUTTERRADIUS 80
+#define OUTTERWIDTHMIN 40
+#define OUTTERWIDTHMAX 80
+#define INNERRADIUS 50
+#define KERN 10
+#define FADESPEED 3.3
+
+// layer style
+void WidgetFB::drawLayers(QPainter &painter) const {
+    //qDebug() << "drawing layer";
     painter.save();
-    painter.translate(x,y);
+    painter.translate(-((NBEATS-1)*(OUTTERRADIUS*2+KERN))/2,0);
 
-    QBrush brush;
-
-    {
-        painter.save();
-        QPen pen;
-        pen.setWidth(width);
-        //pen.setColor(qRgb(255,0,0));
-        painter.setPen(pen);
-        QBrush brush;
-        brush.setStyle(Qt::SolidPattern);
-        brush.setColor("white");
-        painter.setBrush(brush);
-        painter.drawEllipse(-OUTTERRADIUS,-OUTTERRADIUS,2*OUTTERRADIUS,2*OUTTERRADIUS);
-        painter.restore();
+    { // drawing back
+	int index=0;
+	for (Bits::const_iterator iter=bits.begin(); iter!=bits.end(); iter++) {
+	    painter.save();
+	    painter.translate(index*(OUTTERRADIUS*2+KERN),0);
+	    QPen pen;
+	    pen.setWidth(iter->width);
+	    painter.setPen(pen);
+	    painter.drawEllipse(-OUTTERRADIUS,-OUTTERRADIUS,2*OUTTERRADIUS,2*OUTTERRADIUS);
+	    painter.restore();
+	    index++;
+	}
     }
 
-    if (state) {
-        painter.save();
-        QBrush brush;
-        brush.setStyle(Qt::SolidPattern);
-        //brush.setColor(qRgb(255,0,0));
-        brush.setColor(colors[index]);
-        painter.setPen(Qt::NoPen);
-        painter.setBrush(brush);
-        painter.drawEllipse(-INNERRADIUS,-INNERRADIUS,2*INNERRADIUS,2*INNERRADIUS);
-        painter.restore();
+    { // drawing dots
+	int index=0;
+	for (Bits::const_iterator iter=bits.begin(); iter!=bits.end(); iter++) {
+	    painter.save();
+	    painter.translate(index*(OUTTERRADIUS*2+KERN),0);
+	    QBrush brush;
+	    brush.setStyle(Qt::SolidPattern);
+	    brush.setColor("white");
+	    painter.setPen(Qt::NoPen);
+	    painter.setBrush(brush);
+	    painter.drawEllipse(-OUTTERRADIUS,-OUTTERRADIUS,2*OUTTERRADIUS,2*OUTTERRADIUS);
+	    painter.restore();
+	    index++;
+	}
+    }
+
+    { // drawing color
+	int index=0;
+	for (Bits::const_iterator iter=bits.begin(); iter!=bits.end(); iter++) {
+	    if (iter->state) {
+		painter.save();
+		painter.translate(index*(OUTTERRADIUS*2+KERN),0);
+		QBrush brush;
+		brush.setStyle(Qt::SolidPattern);
+		brush.setColor(colors[index]);
+		painter.setPen(Qt::NoPen);
+		painter.setBrush(brush);
+		painter.drawEllipse(-INNERRADIUS,-INNERRADIUS,2*INNERRADIUS,2*INNERRADIUS);
+		painter.restore();
+	    }
+	    index++;
+	}
+    }
+
+    painter.restore();
+}
+
+// default style
+void WidgetFB::drawStatic(QPainter &painter) const {
+    //qDebug() << "drawing static";
+    painter.save();
+    painter.translate(-((NBEATS-1)*(OUTTERRADIUS*2+KERN))/2,0);
+
+    { // drawing back
+	int index=0;
+	for (Bits::const_iterator iter=bits.begin(); iter!=bits.end(); iter++) {
+	    painter.save();
+	    painter.translate(index*(OUTTERRADIUS*2+KERN),0);
+	    QPen pen;
+	    pen.setWidth(5);
+	    painter.setPen(pen);
+	    painter.drawEllipse(-OUTTERRADIUS,-OUTTERRADIUS,2*OUTTERRADIUS,2*OUTTERRADIUS);
+	    painter.restore();
+	    index++;
+	}
+    }
+
+    { // drawing dots
+	int index=0;
+	for (Bits::const_iterator iter=bits.begin(); iter!=bits.end(); iter++) {
+	    painter.save();
+	    painter.translate(index*(OUTTERRADIUS*2+KERN),0);
+	    QBrush brush;
+	    brush.setStyle(Qt::SolidPattern);
+	    brush.setColor("white");
+	    painter.setPen(Qt::NoPen);
+	    painter.setBrush(brush);
+	    painter.drawEllipse(-OUTTERRADIUS,-OUTTERRADIUS,2*OUTTERRADIUS,2*OUTTERRADIUS);
+	    painter.restore();
+	    index++;
+	}
+    }
+
+    { // drawing color
+	int index=0;
+	for (Bits::const_iterator iter=bits.begin(); iter!=bits.end(); iter++) {
+	    if (iter->state) {
+		painter.save();
+		painter.translate(index*(OUTTERRADIUS*2+KERN),0);
+		QBrush brush;
+		brush.setStyle(Qt::SolidPattern);
+		brush.setColor(colors[index]);
+		painter.setPen(Qt::NoPen);
+		painter.setBrush(brush);
+		painter.drawEllipse(-INNERRADIUS,-INNERRADIUS,2*INNERRADIUS,2*INNERRADIUS);
+		painter.restore();
+	    }
+	    index++;
+	}
     }
 
     painter.restore();
@@ -62,14 +144,31 @@ QColor WidgetFB::getColor() const {
     return QColor::fromCmykF(bits[0].state,bits[1].state,bits[2].state,bits[3].state);
 }
 
+void WidgetFB::setStyle(int style) {
+    this->style = style;
+    style %= 2;
+}
+
 void WidgetFB::on_timer_timeout() {
-    qDebug() << "timeout";
+    //qDebug() << "timeout";
     for (Bits::iterator iter=bits.begin(); iter!=bits.end(); iter++) {
-	iter->width--;
+	iter->width-=FADESPEED;
 	if (iter->width<OUTTERWIDTHMIN) iter->width=OUTTERWIDTHMIN;
     }
 	
     update();
+}
+
+void WidgetFB::contextMenuEvent(QContextMenuEvent *event) {
+    //qDebug() << "context";
+    QMenu menu(this);
+    menu.addAction(actionSetStyle);
+    menu.exec(event->globalPos());
+}
+
+void WidgetFB::on_actionSetStyle_activated() {
+    style++;
+    style %= 2;
 }
 
 WidgetFB::WidgetFB(QWidget *parent) : QWidget(parent), style(0) {
@@ -77,6 +176,8 @@ WidgetFB::WidgetFB(QWidget *parent) : QWidget(parent), style(0) {
     for (int k=0; k<NBEATS; k++) {
         bits.append(Bit());
     }
+    actionSetStyle = new QAction("switch style",this);
+    actionSetStyle->setObjectName("actionSetStyle");
     timer = new QTimer(this);
     timer->setObjectName("timer");
     timer->setInterval(50);
@@ -92,14 +193,14 @@ void WidgetFB::keyReleaseEvent(QKeyEvent* /*event*/) {
 
 void WidgetFB::toogleState(int num) {
     bits[num].state = !bits[num].state;
-    qDebug() << bits[num].state << bits[num].width;
+    //qDebug() << bits[num].state << bits[num].width;
     bits[num].width = OUTTERWIDTHMAX;
     //qDebug() << "a" << kk << bits[kk];
     update();
 }
 
 void WidgetFB::keyPressEvent(QKeyEvent* event) {
-    qDebug() << "pressed" << event->key();
+    //qDebug() << "pressed" << event->key();
     int kk=0;
     if (event->key()==65 || event->key()==81) {
         toogleState(kk);
@@ -151,21 +252,13 @@ void WidgetFB::paintEvent(QPaintEvent *event) {
     painter.setRenderHint(QPainter::Antialiasing);
 
     painter.translate(width()/2,height()/2);
-    painter.translate(-((NBEATS-1)*(OUTTERRADIUS*2+KERN))/2,0);
-    qDebug() << "style" << style;
-    if (style==1) {
+    if (style==0) {
+	drawLayers(painter);
     } else {
-	int kk=0;
-	//qDebug() << "state =" << getState();
-	for (Bits::const_iterator iter=bits.begin(); iter!=bits.end(); iter++) {
-	    //qDebug() << kk << *iter;
-	    drawPad(painter,kk,kk*(OUTTERRADIUS*2+KERN),0,iter->state,iter->width);
-	    kk++;
-	}
+	drawStatic(painter);
     }
 }
 
 WidgetFB::Bit::Bit() :
-    state(false),
-    width(OUTTERWIDTHMIN) {
-}
+state(false),
+width(OUTTERWIDTHMIN) { }
--- widgetfb.h
+++ widgetfb.h
@@ -12,25 +12,32 @@ class WidgetFB : public QWidget {
 	WidgetFB(QWidget *parent = NULL);
 	QColor getColor() const;
 	int getState() const;
+    public slots:
+	void setStyle(int style);
     signals:
 	void padPressed(int number);
 	void stateChanged(int state);
 	void stateReleased(int state);
     protected slots:
 	void on_timer_timeout();
+        void on_actionSetStyle_activated();
     protected:
 	virtual void paintEvent(QPaintEvent *event);
 	virtual void keyPressEvent(QKeyEvent *event);
 	virtual void keyReleaseEvent(QKeyEvent *event);
+	virtual void contextMenuEvent(QContextMenuEvent *event);
 	void toogleState(int bit);
+	void drawLayers(QPainter &painter) const;
+	void drawStatic(QPainter &painter) const;
 	struct Bit {
 	    Bit();
 	    bool state;
-	    int width;
+	    float width;
 	};
 	typedef QVector<Bit> Bits;
 	Bits bits;
 	QTimer *timer;
+	QAction *actionSetStyle;
 	int style;
 };
 
